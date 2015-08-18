8dee0e36b787f4f812331d1a1c7bcb0c9b276b6e
floppp <fernandolopezlaso@gmail.com>
2015-08-18 16:13:07 +0200

    Number of primes below a number is correct, but my ouput is not

--- tuenti_challenge/2015/cpp/problem_2.cpp
+++ tuenti_challenge/2015/cpp/problem_2.cpp
@@ -11,13 +11,14 @@ using std::vector;  using std::cout;  using std::cin;
 using std::string;  using std::endl;
 
 template<class T> void print_vector(vector<T>);
+void almost_primes(uint32_t const, uint32_t const, vector<uint32_t> const);
 
 int main()
 {
   std::map<uint32_t, uint32_t> extremes;
   std::pair<uint32_t, uint32_t> pairs;
-  vector<uint32_t> primes;
 
+  // cout << "YOU NEED TO PASS DATA FROM BASH... < data/testInput_2.txt" << endl;
   // Erasing first line with number of tests.
   cin >> pairs.first;
 
@@ -25,14 +26,14 @@ int main()
   while (cin >> pairs.first >> pairs.second)
     extremes.insert(pairs);
 
-  // for (std::pair<uint32_t, uint32_t> pair: extremes)
-  // {
-  //   cout << "Calculating primes between " << pair.first << " and " << pair.second << endl;
-  //   primes = primes::segmented_sieve(pair.first, pair.second);
-  //   // print_vector(primes);
-  // }
-  primes = simple_sieve(100000, primes);
-  print_vector(primes);
+  for (std::pair<uint32_t, uint32_t> pair: extremes)
+  {
+    vector<uint32_t> primes;
+    cout << "Calculating primes between " << pair.first << " and " << pair.second << endl;
+    primes::simple_sieve(pair.second, primes);
+    cout << "\tnumber of primes: " << primes.size() << endl;
+    almost_primes(pair.first, pair.second, primes);
+  }
 
 	return 0;
 }
@@ -44,4 +45,25 @@ void print_vector(vector<T> v)
   cout << endl;
 }
 
+void almost_primes(uint32_t const L, uint32_t const R, vector<uint32_t> const primes)
+{
+  uint32_t len = primes.size();
+  uint32_t count = 0;
+  uint32_t aux = 0;
 
+  // while (primes[i] < L)
+    // ++i;
+  // cout << i << ' ' << primes[i] << endl;
+
+  for (uint32_t i = 0; i < len-1; ++i)
+    for (uint32_t j = i; j < len; j++)
+    {
+      aux = primes[i] * primes[j];
+      if (aux > R)
+        break;
+      else if (aux < L)
+        continue;
+      count++;
+    }
+  cout << count << endl;
+}
